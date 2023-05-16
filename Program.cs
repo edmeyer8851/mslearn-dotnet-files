3e8a9288c67c0094f73387b34765781c5f6f8012
Ed_Meyer <edmeyer8851@gmail.com>
2023-05-16 14:34:12 -0400

    iterate through the sales files of each store, calculate the total sales, and write it to the totals.txt file

--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ Directory.CreateDirectory(salesTotalDir);
 
 var salesFiles = FindFiles(storesDirectory);
 
-// File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 IEnumerable<string> FindFiles(string folderName){
     List<string> salesFiles = new List<string>();
@@ -26,12 +28,22 @@ IEnumerable<string> FindFiles(string folderName){
     return salesFiles;
 }
 
-var salesJson = File.ReadAllText($"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales.json");
-var salesData = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
+double CalculateSalesTotal(IEnumerable<string> salesFiles){
+    double salesTotal = 0;
 
-var data = JsonConvert.DeserializeObject<SalesTotal>(salesJson);
-File.AppendAllText($"salesTotalDir{Path.DirectorySeparatorChar}totals.txt", $"{data.Total}{Environment.NewLine}");
+    //read files loop
+    foreach (var file in salesFiles){
+        //read the contents of the file
+        string salesJson = File.ReadAllText(file);
 
-class SalesTotal{
-    public double Total {get; set;}
+        //parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        //Add the amount found in the Total field to the salesTotal variable
+        salesTotal  += data?.Total ?? 0;
+    }
+
+    return salesTotal;
 }
+
+record SalesData (double Total);
